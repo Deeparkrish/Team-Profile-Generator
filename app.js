3dc9c45e264d3cc5c50b899e4020d7216a5da1a0
Deepa <kosalai17@gmail.com>
2021-07-04 14:14:26 -0700

    got manager details and created  manager object

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generate = require('./lib/generatehtml');
+const Manager = require('./lib/Manager');
+
+
+
+
 function writeTOFile (filenamae ,data)
 {
 
@@ -5,7 +14,75 @@ function writeTOFile (filenamae ,data)
 }
 // TO DO : Prompt user to input data  and upon successful reponse call generate HTML 
 function init(){
+    inquirer
+      .prompt([
+        {
+        type: 'text',
+        name: 'managerName',
+        message: 'Welcome to Teamprofile Generator! Enter the team manager name :',
+        validate: userInput => {
+                if (userInput) {
+                return true;
+                } else {
+                console.log("Please enter the team manager'sname :");
+                return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'managerId',
+            message: "Enter the team manager's  employee-id :",
+            validate: userInput => {
+                if (userInput) {
+                  return true;
+                } else {
+                  console.log("Please enter the team manager's employee-id:");
+                  return false;
+                }
+            }
 
+        },
+        {
+            type: 'text',
+            name: 'managerEmail',
+            message: "Enter the team manager's email address :",
+            validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your e-mail address!');
+                  return false;
+                }
+              },
+              validate: answer =>{
+                const pass = answer.match(/\S+@\S+\.\S+/);
+                if (pass) {
+                  return true;
+                }
+                return "Please enter a valid email address.";
+              }
+        },
+        {
+            type: 'text',
+            name: 'managerNum',
+            message: "Enter the team manager's office number :",
+            validate: userInput => {
+                if (userInput && (parseInt(userInput))&&(userInput.length ===9)) {
+                  return true;
+                } else {
+                  console.log("Please enter the team manager's number- should be a number:");
+                  return false;
+                }
+            }
+        
+        }         
 
+    ])
+      .then(({managerName,managerId,managerEmail,managerNum }) => {
+        this.manager = new Manager(managerName,managerId,managerEmail,managerNum);
+        console.log(this.manager);
+        
+      });
 }
 init ();
